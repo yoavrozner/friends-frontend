c7b8d3873392cedd8add7de227f8eed98c57019d
nogahaskal <nogahaskal@gmail.com>
2021-08-15 14:50:37 +0300

    feat: create search group

---
+++ src/components/inputs/Search.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-text-field
+    v-model="value"
+    solo
+    :label="label"
+    clearable
+    append-icon="mdi-magnify"
+    @keyup.enter.native="$emit('enter', value)"
+    @click:append="$emit('enter', value)"
+  ></v-text-field>
+</template>
+
+<script>
+export default {
+  name: "Search",
+  props: ["label"],
+  data() {
+    return {
+      value: "",
+    };
+  },
+};
+</script>
--- src/views/Groups.vue
+++ src/views/Groups.vue
@@ -1,12 +1,29 @@
 <template>
   <div>
-    <Header :txt="$t('searchGroups')" />
+    <Header :txt="$t('searchGroup')" />
+    <div id="search-container">
+      <Search :label="$t('searchGroupLabel')" @enter="onEnter" />
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+import Header from "@/components/common/Header";
+import Search from "@/components/inputs/Search";
+
+export default {
+  name: "Groups",
+  components: { Header, Search },
+  methods: {
+    onEnter(value) {
+      console.log(value);
+    },
+  },
+};
 </script>
 
 <style scoped>
+#search-container {
+  margin: 30px 200px;
+}
 </style>
