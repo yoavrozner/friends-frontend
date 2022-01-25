1f56809606c4235bc16c9ff5ba49482fd0fa01c3
beni muler <benimuler100@gmail.com>
2022-01-25 20:50:46 +0200

    fix: errors

--- src/components/group/GroupForm.vue
+++ src/components/group/GroupForm.vue
@@ -105,7 +105,9 @@ export default {
   },
   methods: {
     isCompleteDisabled() {
-      return !this.valid || this.checkValidationMembers() != null || !this.isGroupNameValid || !this.selectedApprovals;
+    console.log('isCompleteDisabled')
+    console.log(this.valid , this.checkValidationMembers() , this.isGroupNameValid , this.selectedApprovals)
+      return !this.valid || this.checkValidationMembers() != null || !this.isGroupNameValid || this.selectedApprovals.length===0;
     },
     checkValidation() {
       return this.isGroupNameValid ? null : this.$t("group.create.groupNameAlreadyExists");
